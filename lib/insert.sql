2ba2ce8c519ba6e27fda677b3035e74b25719790
Koby Fink <finkkoby@icloud.com>
2024-03-25 14:13:34 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'M', 'dark brown', 'sensitive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'brown', 'rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', NULL, 'F', 'black', 'protective', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant', 11, 'F', 'dark brown', 'protective', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'dark brown', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', NULL, 'M', 'light brown', 'rowdy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 12, 'F', 'black', 'friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'M', 'black', NULL, 1);
